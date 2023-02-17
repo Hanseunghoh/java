80f249ac15f93c0d873bcda98c25f6f23c5f80af
Hanseunghoh <123627848+Hanseunghoh@users.noreply.github.com>
2023-02-17 19:24:11 +0900

    Create StringConcat.java

---
+++ 20230217/StringConcat.java
@@ -0,0 +1,19 @@
+
+public class StringConcat {
+
+	public static void main(String[] args) {
+		String st1 = "Coffee";
+		String st2 = "Bread";
+		
+		String st3 = st1.concat(st2);
+		System.out.println(st3);
+		
+		String st4 = "Fresh".concat(st3);
+		System.out.println(st4);
+
+	}
+
+}
+
+
+//클래스에 생략된 extends object의 Stringd의 concat메서드사용
