368a331881d16119a05113a33955e1d6dee31303
Hanseunghoh <123627848+Hanseunghoh@users.noreply.github.com>
2023-02-20 19:07:35 +0900

    Create Sample.java

---
+++ 20230220/Sample.java
@@ -0,0 +1,16 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+		char ch1 = str.charAt(0); // 첫 번째 문자 추출
+		char ch2 = str.charAt(1); // 두 번째 문자 추출
+		int len = str.length(); //문자열 길이 반환
+		
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(len);
+
+	}
+
+}
