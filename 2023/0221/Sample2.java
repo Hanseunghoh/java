d4969cfee302aaf6112ca69e95d9d83a805ebf27
Hanseunghoh <123627848+Hanseunghoh@users.noreply.github.com>
2023-02-21 17:56:13 +0900

    Create Sample2.java

---
+++ 2023/0221/Sample2.java
@@ -0,0 +1,16 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int[] test;
+		test = new int[5];
+		System.out.println("test[10]에 값을 대입합니다");
+		test[10] = 80;
+		System.out.println("test[10]에 80을 대입했습니다");
+		System.out.println("무사히 종료했습니다");
+
+	}
+
+}
+
+//5칸짜리 상자인데 10번째칸=80 이므로 에러
